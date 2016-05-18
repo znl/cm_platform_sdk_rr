fa638cd2b98438000957e25a9c8961454406a59a
paranoiagu <gu.yanfeng@gmail.com>
2016-05-18 12:51:03 +0800

    Merge remote-tracking branch 'ResurrectionRemix/marshmallow' into marshmallow

--- sdk/src/java/cyanogenmod/providers/CMSettings.java
+++ sdk/src/java/cyanogenmod/providers/CMSettings.java
@@ -2760,7 +2760,13 @@ public final class CMSettings {
           * @hide
           */
         public static final String CM_SETUP_WIZARD_COMPLETED = "cm_setup_wizard_completed";
- 
+
+        /**
+         * Whether lock screen bluring is enabled on devices that support this feature
+         * @hide
+         */
+        public static final String LOCK_SCREEN_BLUR_ENABLED = "lock_screen_blur_enabled";
+
         // endregion
 
         /**
